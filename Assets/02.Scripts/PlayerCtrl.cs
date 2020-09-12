ea67fe04d1fb40dae02ff9dd74d57f1965908c36
Lee JaeHyun <myleje@gmail.com>
2020-09-12 14:34:26 +0900

    .

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class PlayerCtrl : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float h = Input.GetAxis("Horizontal");
+
+        Debug.Log("h="+h);    
     }
 }
