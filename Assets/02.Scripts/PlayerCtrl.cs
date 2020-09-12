86ac0a19c7b3035ddaeea45a85c9b769e05d2ac0
Lee JaeHyun <myleje@gmail.com>
2020-09-12 15:29:06 +0900

    .

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerCtrl : MonoBehaviour
 {
     public float moveSpeed = 50.0f;
+    public float turnSpeed = 100.0f;
 
     void Start()
     {
@@ -15,10 +16,16 @@ public class PlayerCtrl : MonoBehaviour
     {
         float h = Input.GetAxis("Horizontal"); // -1.0f ~ 0.0f ~ 1.0f
         float v = Input.GetAxis("Vertical");   // -1.0f ~ 0.0f ~ 1.0f 
+        float r = Input.GetAxis("Mouse X");    //마우스 좌우 이동 변위값
 
-        //transform.position += new Vector3(0, 0, 0.1f * v);
-        transform.Translate( Vector3.forward * moveSpeed * Time.deltaTime * v ); //(방향 * 속도 * 변위)
-        transform.Translate( Vector3.right * moveSpeed * Time.deltaTime * h );   //
+        //이동로직
+        Vector3 dir = (Vector3.forward * v) + (Vector3.right * h);
+        transform.Translate(dir.normalized * Time.deltaTime * moveSpeed);
+        //회전로직
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * r);
+
+        // transform.Translate( Vector3.forward * moveSpeed * Time.deltaTime * v ); //(방향 * 속도 * 변위)
+        // transform.Translate( Vector3.right * moveSpeed * Time.deltaTime * h );   //
 
         /* 정규화 벡터(Normalized Vector), 단위 벡터(Unit Vector)
             Vector3.forward  = new Vector3(0, 0, 1)
@@ -29,6 +36,6 @@ public class PlayerCtrl : MonoBehaviour
             Vector3.one      = new Vector3(1, 1, 1)
         */
 
-        Debug.Log("h=" + h + "v=" + v);
+        //Debug.Log("h=" + h + "v=" + v);
     }
 }
