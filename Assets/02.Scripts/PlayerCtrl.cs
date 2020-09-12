45f6e917849c226339ccd67c02002e42c6f4a346
Lee JaeHyun <myleje@gmail.com>
2020-09-12 14:42:29 +0900

    .

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -12,9 +12,9 @@ public class PlayerCtrl : MonoBehaviour
     void Update()
     {
         float h = Input.GetAxis("Horizontal"); // -1.0f ~ 0.0f ~ 1.0f
-        float v = Input.GetAxis("Vertical");
+        float v = Input.GetAxis("Vertical");   // -1.0f ~ 0.0f ~ 1.0f 
 
-        transform.position += new Vector3(0, 0, 0.1f * h);
+        transform.position += new Vector3(0, 0, 0.1f * v);
 
         Debug.Log("h=" + h + "v=" + v);
     }
