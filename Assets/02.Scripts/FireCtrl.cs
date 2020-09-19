24fbd2fea698e76c31283272fd1bed60b0c03b40
Lee JaeHyun <myleje@gmail.com>
2020-09-19 09:43:15 +0900

    .

--- Assets/02.Scripts/FireCtrl.cs
+++ Assets/02.Scripts/FireCtrl.cs
@@ -15,6 +15,9 @@ public class FireCtrl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))  //0: Left button  , 1: Right Button , 2: Middle
+        {
+            Instantiate(bulletPrefab, firePos.position, firePos.rotation);
+        }
     }
 }
