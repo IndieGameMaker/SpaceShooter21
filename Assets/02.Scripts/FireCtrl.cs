0881c5381400720a12c802ae0854f3111b916ced
Lee JaeHyun <myleje@gmail.com>
2020-09-19 16:31:59 +0900

    .

--- Assets/02.Scripts/FireCtrl.cs
+++ Assets/02.Scripts/FireCtrl.cs
@@ -46,9 +46,22 @@ public class FireCtrl : MonoBehaviour
         float scale = Random.Range(1.0f, 3.0f);
         muzzleFlash.transform.localScale = Vector3.one * scale;
 
+        //MuzzleFlash Texture Offset 변경
+        Vector2 offset = new Vector2(Random.Range(0, 2), Random.Range(0, 2)) * 0.5f ; //(0, 0) (1, 0) (1, 1) (0, 1) / 2
+        muzzleFlash.material.SetTextureOffset("_MainTex", offset);
+        //muzzleFlash.material.mainTextureOffset = offset;
+
         muzzleFlash.enabled = true;
         yield return new WaitForSeconds(0.2f);
         muzzleFlash.enabled = false;
     }
+
+    /*
+    Random.Range 함수
+
+    int 파라메터   : Random.Range(0, 10)        => 0 ~ 9
+    float 파라메터 : Random.Range(0.0f, 10.0f)  => 0.0f ~ 10.0f
+    */
+
 
 }
