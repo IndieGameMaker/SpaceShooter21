28c290091609f3b19a2116ad68cd593bc02a0c60
Lee JaeHyun <myleje@gmail.com>
2020-09-26 14:31:50 +0900

    .

---
+++ Assets/02.Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //몬스터가 출현할 위치정보
+    public Transform[] points;
+
+    //몬스터 프리팹
+    public GameObject monsterPrefab;
+
+    //몬스터의 출현 간격(생성 간격)
+    public float createTime = 3.0f;
+
+    void Start()
+    {
+        GameObject spawnPointGroup = GameObject.Find("SpawnPointGroup");  
+        if (spawnPointGroup != null)
+        {
+            points = spawnPointGroup.GetComponentsInChildren<Transform>();
+        }      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
