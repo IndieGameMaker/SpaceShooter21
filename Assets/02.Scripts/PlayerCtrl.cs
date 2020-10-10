5eb27ad68a20a20ea385a9ebe16e6bcaa118112f
Lee JaeHyun <myleje@gmail.com>
2020-10-10 11:33:23 +0900

    .

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [System.Serializable]
 public class PlayerAnim
@@ -19,6 +20,7 @@ public class PlayerCtrl : MonoBehaviour
     public float turnSpeed = 100.0f;
 
     public PlayerAnim playerAnim;
+    public Image hpBar;
 
     private Animation animation;
 
