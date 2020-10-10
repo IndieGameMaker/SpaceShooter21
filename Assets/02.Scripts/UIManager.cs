e8c0c9aed6acd1c56b966eb7f7f2a0d8dd076025
Lee JaeHyun <myleje@gmail.com>
2020-10-10 10:56:52 +0900

    .

---
+++ Assets/02.Scripts/UIManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public void OnClickStart()
+    {
+        //씬 로딩
+        SceneManager.LoadScene("Level01");
+        SceneManager.LoadScene("Game" , LoadSceneMode.Additive);
+    }
+}
